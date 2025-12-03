d293be5f6e64e08eaf681a5ebd87949c4a9cadd2
benjaminwfsdf <benjagonzalito08@gmail.com>
2025-12-03 09:36:48 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "v3.1.89";   // ← CAMBIA ESTO cuando quieras forzar update
+const VERSION = "v3.2.35";   // ← CAMBIA ESTO cuando quieras forzar update
 
 const PRECACHE = `precache-${VERSION}`;
 const RUNTIME  = `runtime-${VERSION}`;
